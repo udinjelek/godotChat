4e09f0f6369affca6f9b8e2a5ba12937de578c08
udin jelek <udinjelek@users.noreply.github.com>
2021-09-01 06:14:46 +0700

    update from work

--- script/dialogSys.gd
+++ script/dialogSys.gd
@@ -22,7 +22,7 @@ var	chatJump
 var	chatReturnValue
 var	chatText
 
-var choiceChatJump = [-1,-1,-1]
+var choiceChatJump = [null,-1,-1,-1]
 
 var valid_nextLinePoint
 var splashScreenMode = 0 
@@ -40,7 +40,7 @@ func _process(_delta):
 			mouseClickBtn = false
 			nextProcess()
 	if splashScreenMode != 0 :
-		print($BlackScreen.rect_position.x)
+#		print($BlackScreen.rect_position.x)
 		$BlackScreen.rect_position.x -= 35
 		if $BlackScreen.rect_position.x <= -500 and splashScreenMode == 1:
 			splashScreenMode = 2
@@ -64,8 +64,14 @@ func startProcess(chatGlobalCodeUse):
 
 
 func nextProcess():
-#	print(maxChatNo, " ",chatNo," ",maxChatLine , " ", chatLine )
-	
+	#	print(choiceChatJump)
+	# jika ada jump point dari step sebelumnya, maka jump di lakukan di sini, 
+	# sehingga generate output chat sudah sesuai dengan data terbaru
+	if choiceChatJump[0] != null :
+		calculateJumpPoint()
+		choiceChatJump[0] = null 
+		
+	#	print(maxChatNo, " ",chatNo," ",maxChatLine , " ", chatLine )
 	valid_nextLinePoint = false
 	calculateNextLinePoint()
 	if valid_nextLinePoint == true:
@@ -95,8 +101,9 @@ func generateChatOutput():
 	if chatType == "normal":
 		$TextBox/ChatCharName.text	= LoaderText.getChatCharName(chatGlobalCode,chatNo,chatLine)
 		$TextBox/ChatText.text	= LoaderText.getChatText(chatGlobalCode,chatNo,chatLine)
+		print(LoaderText.checkKeyChatLineExist("chatJump",chatGlobalCode,chatNo,chatLine))
 	elif chatType == "question":
-		print(chatGlobalCode,chatNo,chatLine)
+#		print(chatGlobalCode,chatNo,chatLine)
 		$TextBox/ChatCharName.text	= LoaderText.getChatCharName(chatGlobalCode,chatNo,chatLine)
 		$TextBox/ChatText.text	= LoaderText.getChatText(chatGlobalCode,chatNo,chatLine)
 		generateChoiceOutput()
@@ -107,9 +114,9 @@ func generateChatOutput():
 		
 func generateChoiceOutput():
 	for arrChoice in range(1,maxChatLine + 1):
-		get_node("Choice0" + str(arrChoice - 1)).visible = true
-		get_node("Choice0" + str(arrChoice - 1) + "/ChatText").text =  LoaderText.getChatText(chatGlobalCode,chatNo,arrChoice)
-		choiceChatJump[arrChoice - 1] =  int(LoaderText.getChatJump(chatGlobalCode,chatNo,arrChoice))
+		get_node("Choice0" + str(arrChoice)).visible = true
+		get_node("Choice0" + str(arrChoice) + "/ChatText").text =  LoaderText.getChatText(chatGlobalCode,chatNo,arrChoice)
+		choiceChatJump[arrChoice] =  int(LoaderText.getChatJump(chatGlobalCode,chatNo,arrChoice))
 
 func _on_NextBtn_button_down():
 	mouseClickBtn = true
@@ -118,14 +125,16 @@ func _on_NextBtn_button_up():
 	mouseClickBtn = false
 
 func _on_ChoiceBtn_sendChoiceAnswered(obj):
+	
 	hideAllChoice()
 	var choiceAnswered:int = int(obj[-1])
 #	send value array choiceChatJump, based on button click,  int(obj[-1]) -> right 1 of choiceBtn Name Clicked-> noChoiceClicked
-	chatJump = choiceChatJump[choiceAnswered]
-	calculateJumpPoint()
+	choiceChatJump[0] = choiceChatJump[choiceAnswered]
+	
 	nextProcess()
 	
 func calculateJumpPoint():
+	chatJump = choiceChatJump[0]
 	if chatJump == -1:
 		quitProcess()
 	else:
@@ -135,7 +144,7 @@ func calculateJumpPoint():
 		chatLine = -1
 		
 func hideAllChoice():
-	for arrChoice in range(0,3):
+	for arrChoice in range(1,4):
 		get_node("Choice0" + str(arrChoice) + "/ChatText").text =  "-"
 		get_node("Choice0" + str(arrChoice )).visible = false
 		
